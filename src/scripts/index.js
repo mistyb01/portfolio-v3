5e67859cc8c46a52e1ef79e100a7b499452a62f9
Misty <60805050+mistyb01@users.noreply.github.com>
2023-09-01 14:52:10 -0700

    Dark mode toggle (#13)
    * added dark/light mode icons
    
    * correct icon is shown on different modes
    
    * clicking button toggles mode
    
    * color mode controlled by javascript
    
    * positioning toggle button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-
+// ON-SCROLL ANIMATION
 const sectionObserver = new IntersectionObserver((sections)=>{
     sections.forEach(section => {
             if (section.isIntersecting) {
@@ -12,3 +12,36 @@ document.querySelectorAll('.animate').forEach((i) => {
         sectionObserver.observe(i);
     }
 })
+
+// LIGHT/DARK MODE TOGGLE
+const modeBtn = document.querySelector('.btn--color-mode');
+
+const modeIcon = document.createElement('span')
+modeIcon.classList.add('material-symbols-outlined')
+
+let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+function setMode() {
+    if (isDarkMode) {
+        modeIcon.innerText = 'light_mode' 
+        document.body.classList.add('dark-mode')
+    } else {
+        modeIcon.innerText = 'dark_mode'
+        document.body.classList.add('light-mode')
+    }     
+}
+
+// watch for changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    isDarkMode = event.matches;
+    setMode();
+});
+
+modeBtn.addEventListener('click', () => {
+    document.body.className = ""
+    isDarkMode = !isDarkMode;
+    setMode();
+})
+
+modeBtn.appendChild(modeIcon);
+setMode();
