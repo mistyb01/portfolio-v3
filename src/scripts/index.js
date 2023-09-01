64e4b4917366ad69f0209490ed0f6c7d3843aee1
Misty <60805050+mistyb01@users.noreply.github.com>
2023-08-31 18:38:32 -0700

    Polish home design (#8)
    * tech section grid
    
    * removed svg header and footer
    
    the simplicity actually feels pretty good
    
    * fade-in animation on scroll
    
    * remove unused class, spacing fix

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+
+const sectionObserver = new IntersectionObserver((sections)=>{
+    sections.forEach(section => {
+            if (section.isIntersecting) {
+                section.target.classList.add("active");
+            } 
+    })
+}, {threshold: 0.1})
+
+document.querySelectorAll('.animate').forEach((i) => {
+    if (i) {
+        sectionObserver.observe(i);
+    }
+})
