962d969ce0e42be46629f7f0a59fbb9f451119e5
Misty Becker <mistyb01@uw.edu>
2023-09-01 13:17:51 -0700

    correct icon is shown on different modes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-
+// ON-SCROLL ANIMATION
 const sectionObserver = new IntersectionObserver((sections)=>{
     sections.forEach(section => {
             if (section.isIntersecting) {
@@ -12,3 +12,21 @@ document.querySelectorAll('.animate').forEach((i) => {
         sectionObserver.observe(i);
     }
 })
+
+// LIGHT/DARK MODE TOGGLE
+const modeBtn = document.querySelector('.btn--color-mode');
+
+const modeIcon = document.createElement('span')
+modeIcon.classList.add('material-symbols-outlined')
+
+let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+isDarkMode ? modeIcon.innerText = 'light_mode' : modeIcon.innerText = 'dark_mode'
+
+// watch for changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    isDarkMode = event.matches;
+    isDarkMode ? modeIcon.innerText = 'light_mode' : modeIcon.innerText = 'dark_mode'
+    
+});
+
+modeBtn.appendChild(modeIcon)
