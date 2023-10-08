1d42b8f8fcd5256e408e43f63aaf8f694fc7e6d5
Misty <60805050+mistyb01@users.noreply.github.com>
2023-10-07 19:23:10 -0700

    dark mode toggle and various small ui fixes (#16)
    * change button colors
    
    * change font display to block
    
    * remove animation on scroll
    
    * fix font preload
    
    * upload light and dark mode svg
    
    * implement dark mode toggle
    
    * remove transition delay so toggle is smooth
    
    * position toggle button
    
    * use svg-inject library to style svg
    
    * change color mode icon on toggle
    
    * style toggle button
    
    * update form spacing and text style
    
    * highlight form field on focus

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,3 +13,33 @@ document.querySelectorAll('.animate').forEach((i) => {
     }
 })
 
+// DARK/LIGHT MODE TOGGLE
+const modeToggleBtn = document.querySelector('.mode-toggle-btn');
+const modeToggleImg = document.getElementById('mode-toggle-img');
+
+modeToggleBtn.addEventListener('click', () => {
+        modeToggleBtn.replaceChildren();
+
+        const svg = document.createElement('img');
+        svg.setAttribute('onload', 'SVGInject(this)');
+        
+        document.body.classList.toggle('dark');
+        createColorModeSVG();
+    }
+)
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark');
+}
+
+function createColorModeSVG() {
+    const svg = document.createElement('img');
+    svg.setAttribute('onload', 'SVGInject(this)');
+    if (document.body.classList.contains('dark')) {
+        svg.src = "../assets/svg/icon-darkmode.svg";
+    } else {
+        svg.src = "../assets/svg/icon-lightmode.svg";
+    }
+    modeToggleBtn.appendChild(svg);
+}
+createColorModeSVG();
